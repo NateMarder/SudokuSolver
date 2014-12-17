38589d706210ac44542b8314881b035a81ad64e5
Nate Marder <ntmarder@gmail.com>
2014-12-17 10:46:23 -1000

    Java Implementation

--- Java_SudokuSolver.java
+++ Java_SudokuSolver.java
@@ -3,7 +3,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class JavaRecursiveSudoku {
+public class Java_SudokuSolver {
 
    private static int[][] theboard;
    private final static String FILENAME = "input.txt";
