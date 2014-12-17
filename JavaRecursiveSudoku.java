22dfb9e61368e6e6088d2aa71328cf3ea24ba433
Nate Marder <ntmarder@gmail.com>
2014-12-17 10:27:47 -1000

    Update and rename RecursiveSudoku to JavaRecursiveSudoku.java

--- JavaRecursiveSudoku.java
+++ JavaRecursiveSudoku.java
@@ -3,7 +3,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class RecursiveSudoku {
+public class JavaRecursiveSudoku {
 
    private static int[][] theboard;
    private final static String FILENAME = "input.txt";
